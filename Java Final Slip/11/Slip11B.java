0bd2c70ff9fea0ba7869501583e83f17f97a9a56
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-10-15 09:43:31 +0530

    added graphics slip for java

--- Java Final Slip/11/Slip11B.java
+++ Java Final Slip/11/Slip11B.java
@@ -1,40 +1,30 @@
-import java.awt.Color;
-import java.awt.Graphics;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
 import java.util.*;
 
 class Lamp extends JPanel {
-  @Override
-  protected void paintComponent(Graphics g) {
+  public void paintComponent(Graphics g) {
     super.paintComponent(g);
-    Random rand = new Random();
-    int r = rand.nextInt(256);
-    int gr = rand.nextInt(256);
-    int b = rand.nextInt(256);
-    Color c1 = new Color(r, gr, b);
-    g.drawRect(0, 250, 290, 29);
-    g.drawLine(125, 250, 125, 160);
-    g.drawLine(175, 250, 175, 160);
-    g.drawArc(85, 157, 130, 50, -65, 312);
-    g.drawArc(85, 87, 130, 50, 62, 58);
-    g.drawLine(85, 177, 119, 89);
-    g.drawLine(215, 177, 181, 89);
-    g.setColor(c1);
-    g.fillArc(78, 120, 40, 40, 63, -174);
-    g.fillOval(120, 96, 40, 40);
-    g.fillArc(173, 100, 40, 40, 110, 100);
+
+    Random random = new Random();
+    Color newColor = new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+
+    g.setColor(Color.DARK_GRAY);
+    g.fillRect(140, 200, 110, 20);
+    g.fillRect(180, 120, 20, 80);
+
+    g.setColor(newColor);
+    g.fillRect(140, 80, 110, 50);
   }
 }
 
-public class Slip11B {
+class Slip11B {
   public static void main(String[] args) {
+    JFrame frame = new JFrame();
     Lamp panel = new Lamp();
-    JFrame app = new JFrame();
-    app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    app.add(panel);
-    app.setSize(300, 300);
-    app.setTitle("Table Lamp");
-    app.setVisible(true);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.add(panel);
+    frame.setSize(400, 400);
+    frame.setVisible(true);
   }
 }
